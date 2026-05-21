a581a0b4d2bf742685ef2fe6ac2dfff8b9a33817
Dogan Akcicek <akcidogan@gmail.com>
2026-05-21 11:53:19 +0200

    Fehlerbehebung

--- php/src/my_events.php
+++ php/src/my_events.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/header.php';
 require_once __DIR__ . '/includes/footer.php';
-require_once __DIR__ . '/includes/my_events_service.php';
+require_once __DIR__ . '/includes/my_events_function.php';
 require_once __DIR__ . '/includes/my_events_page.php';
 
 if (!isLoggedIn()) {
@@ -17,22 +17,3 @@ $my_events = getMyEventsForUser($db, $user_id);
 $weekdays = getGermanWeekdays();
 
 renderMyEventsPage($my_events, $weekdays);
->
-            </div>
-        <?php endif; ?>
-    </div>
-
-    <footer class="footer">
-        <p>&copy; <?php echo date('Y'); ?> EVENT-SYSTEM. Alle Rechte vorbehalten.</p>
-    </footer>
-
-    <script>
-        setTimeout(function() {
-            let notifications = document.querySelectorAll('.notification');
-            notifications.forEach(function(n) {
-                n.remove();
-            });
-        }, 4000);
-    </script>
-</body>
-</html>
